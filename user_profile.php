4004111a182d53bdd2d1d2f84ec941b371f428f5
phiiileo <emmanuelboluwajib1@gmail.com>
2019-11-11 12:56:48 +0100

    changed to bs4 on user_profile

--- user_profile.php
+++ user_profile.php
@@ -1,220 +1,228 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Home | tifi.tv</title>
-            <link rel="stylesheet" type="text/css"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-            <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-            <link rel="stylesheet" href="css/site_header_footer.css">    </head>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User Profile | tifi.tv</title>
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/site_header_footer.css">
+    <link rel="stylesheet" href="css/user_profile.css">
+</head>
 
 <body>
-
-    <main class="container">    
-        <?php include "components/site_header.php" ?>
-
-        <section class="section--one">
-            <span>
-                <h3>Instantly watch TV shows & Movies</h3>
-            </span>
-            <div class="steps">
-                <section>
-                    <p>STEP 1</p>
-                    <P>Create Account</P>
-                </section>
-                <section class="active">
-                    <p>STEP 2</p>
-                    <p>Welcome to tifi.tv</p>
-                </section>
-            </div>
-        </section>
-
-        <section class="body">
-            <p class="intro">
-                <span>Welcome </span><a href="#">Brandon</a>, Tifi.tv works better when we know what you like
-            </p>
-            <div class="add_something">
-                <p>Add something to your watchlist</p>
-                <input type="text" name="add_something">
-            </div>
-
-            <div class="pick_genre">
-                <p>Pick a genre</p>
-                <div>
-                <article>
-                    <span></span>
-                    <p>Drama</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Romance</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Comedy</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Herror</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Thriller</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p class="tlong">Science Fiction</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Action</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Western</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Adventure</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Animation</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p class="tlong">Documentary</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Fantasy</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Musicals</p>
-                </article>
-                <article>
-                    <span></span>
-                    <p>Crimes</p>
-                </article>
+    <?php include "components/site_header.php" ?>
+    <main>
+        <div class="container">
+            <section class="section--one">
+                <span>
+                    <h3>Instantly watch TV shows & Movies</h3>
+                </span>
+                <div class="steps row">
+                    <section class="col-6">
+                        <p>STEP 1</p>
+                        <P>Create Account</P>
+                    </section>
+                    <section class="active col-6">
+                        <p>STEP 2</p>
+                        <p>Welcome to tifi.tv</p>
+                    </section>
                 </div>
-            </div>
+            </section>
 
-            <div class="select_movies">
-                <p> Select movies 
-                    <span>This allows us to figure out a recomendation for you</span>
+            <section class="body">
+                <p class="intro">
+                    <span>Welcome </span><a href="#">Brandon</a>, Tifi.tv works better when we know what you like
                 </p>
-                <section class="movie_container">
-                    <div class="movie_lists">
-                        <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                        <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                        <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                         <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                        <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                        <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                         <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                        <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                        <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                         <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                        <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
-                        <article class="move_list_item">
-                            <figure>
-                                <img src="uploads/super.jpeg" alt="">
-                            </figure>
-                            <figcaption>
-                                The Unicorn <span>(2019)</span>
-                            </figcaption>
-                        </article>
+                <div class="add_something">
+                    <p>Add something to your watchlist</p>
+                    <input type="text" name="add_something">
+                </div>
 
-                    </div>   
-                </section> 
-            </div>                
-            <p class="cta"><a href="#">Finish</a><a href="#">Skip this step</a></p>
+                <div class="pick_genre">
+                    <p>Pick a genre</p>
+                    <div>
+                        <section>
+                            <article>
+                                <span></span>
+                                <p>Drama</p>
+                            </article>
+                            <article>
+                                <span></span>
+                                <p>Romance</p>
+                            </article>
+                            <article>
+                                <span></span>
+                                <p>Comedy</p>
+                            </article>
+                            <article>
+                                <span></span>
+                                <p>Herror</p>
+                            </article>
+                            <article>
+                                <span></span>
+                                <p>Thriller</p>
+                            </article>
+                            <article class="tlong l">
+                                <span></span>
+                                <p>Science Fiction</p>
+                            </article>
+                            <article>
+                                <span></span>
+                                <p>Action</p>
+                            </article>
+                        </section>
+                        <section>
+                            <article>
+                                <span></span>
+                                <p>Western</p>
+                            </article>
+                            <article>
+                                <span></span>
+                                <p>Adventure</p>
+                            </article>
+                            <article>
+                                <span></span>
+                                <p>Animation</p>
+                            </article>
+                            <article class="tlong">
+                                <span></span>
+                                <p>Documentary</p>
+                            </article>
+                            <article>
+                                <span></span>
+                                <p>Fantasy</p>
+                            </article>
+                            <article>
+                                <span></span>
+                                <p>Musicals</p>
+                            </article>
+                            <article>
+                                <span></span>
+                                <p>Crimes</p>
+                            </article>
+                        </section>
+                    </div>
+                </div>
+
+                <div class="select_movies">
+                    <p> Select movies
+                        <span>This allows us to figure out a recomendation for you</span>
+                    </p>
+                    <section class="movie_container">
+
+                        <div class="movie_lists">
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+                            <article class="move_list_item">
+                                <figure>
+                                    <img src="uploads/super.jpeg" alt="">
+                                </figure>
+                                <figcaption>
+                                    The Unicorn <span>(2019)</span>
+                                </figcaption>
+                            </article>
+
+                        </div>
+                    </section>
+                </div>
+                <p class="cta"><a href="#">Finish</a><a href="#">Skip this step</a></p>
 
             </section>
+        </div>
     </main>
 
     <?php include "components/site_footer.php" ?>
