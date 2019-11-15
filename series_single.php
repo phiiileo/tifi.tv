7e677a440ec25e903efcfd2e6bf67c2e931709c4
phiiileo <emmanuelboluwajib1@gmail.com>
2019-11-15 02:04:42 +0100

    built series_movies.php

---
+++ series_single.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Series | tifi.tv</title>
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/series_single.css">
+</head>
+
+<body>
+    <?php include "components/site_header.php" ?>
+    <main>
+        <section class="page_header border border-dark">
+            <section class="container">
+                <article class="page_header_content">
+                    <div class="vote_number">
+                        <p class="font-weight-bold mb-n1">4/5</p>
+                        <span class="">10k votes</span>
+                    </div>
+                    <div class="vote_star my-3">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i>
+                    </div>
+                    <div class="m_provider d-flex flex-row text-center mb-n4">
+                        <article class="mr-2"><img src="uploads/prov1.png" alt=""><span>9.3/10</span></article>
+                        <article class="mr-2"><img src="uploads/prov3.png" alt=""><span>8.5/10</span></article>
+                        <article class="mr-2"><img src="uploads/prov3.png" alt=""><span>7/10</span></article>
+                        <article class="mr-2"><img src="uploads/prov4.png" alt=""><span>9.8/10</span></article>
+                    </div>
+                </article>
+            </section>
+        </section>
+        <section class="page_body container pl-2">
+            <section class="row mx-auto my-2">
+                <div class=" sidebar col-md-4 pl-0">
+                    <section class="img_container">
+                        <img src="uploads/got.png" alt="" class="">
+                        <figcaption class="m_provider_logo row mx-auto">
+                            <img src="uploads/hbo.png" alt="" class=" col-6 img-fluid">
+                            <img src="uploads/hbo.png" alt="" class=" col-6 img-fluid">
+                        </figcaption>
+                    </section>
+                    <section class="movie_details my-5 py-3">
+                        <p><span>STATUS: </span><span>Ended</span></p>
+                        <p><span>RUNTIME: </span><span>60mins</span> </p>
+                        <p><span>COUNTRY: </span><span>United States</span></p>
+                        <p><span>GENRES: </span><span>Drama, Fantasy, Science Fiction, Action, Adventure</span></p>
+                        <p><span>TOTAL RUNTIME: </span><span>3days, 2 hours, 10 mins (73 episodes)</span></p>
+                        <p><span>LANGUAGE: </span><span>English</span></p>
+                        <p><span>NETWORK: </span><span>HBO</span></p>
+                        <p><span>PREMIERED: </span><span>Apr 18, 2011</span></p>
+                    </section>
+                    <section class="movie_trailer">
+                        <p class="p-2">TRAILER</p>
+                        <iframe src="https://www.youtube.com/embed/zkhUealkTc8" frameborder="0"></iframe>
+                        <article class="d-flex flex-row flex-wrap justify-content-between mt-5">
+                            <img src="uploads/got_throne.png" class="mb-1">
+                            <iframe src="https://www.youtube.com/embed/zkhUealkTc8" frameborder="0"
+                                class="mb-1"></iframe>
+                            <iframe src="https://www.youtube.com/embed/zkhUealkTc8/" frameborder="0"
+                                class="mb-1"></iframe>
+                            </iframe>
+                            <img src="uploads/got_throne.png" class="mb-1">
+                        </article>
+                    </section>
+                    <section class="movie_cc my-5">
+                        <h3>CAST & CREW</h3>
+                        <div class="d-flex flex-row flex-wrap justify-content-between">
+                            <article>
+                                <p>Úrsula Corberó</p>
+                                <p>Tokyo (Silene Oliveira)
+                                    Berlin(Andres de Fonollosa</p>
+                            </article>
+                            <article>
+                                <p>Pedro Alonso</p>
+                                <p>Berlin (Andres de Fonollosa)</p>
+                            </article>
+
+                            <article>
+                                <p>Álvaro Morte</p>
+                                <p>The Professor / Salvador "Salva" Martin (Sergio Marquina)</p>
+                            </article>
+                            <article>
+                                <p>Alba Flores</p>
+                                <p>Nairobi (Agata Jimenez)</p>
+                            </article>
+                        </div>
+                        <div class="border border-top-dark mt-3 pt-3">
+                            <p class="font-weight-bold">See 3 more <i class="fas fa-angle-down"></i></p>
+                        </div>
+                    </section>
+                </div>
+
+                <div class="main_container col-md-8 pt-5">
+                    <h3> Game of Thrones <span>(2011)</span></h3>
+                    <p class="bg_color add_to px-3 py-2"> <i class="fa fa-plus"></i> ADD TO WATCHLIST</p>
+                    <section class="about_movie my-5">
+                        <h5>SYNOPSIS</h5>
+                        <p>
+                            Seven noble families fight for control of the mythical land of Westeros. Friction between
+                            the houses leads to full-scale war. All while a very ancient evil awakens in the farthest
+                            north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that
+                            stands between the realms of men and the icy horrors beyond.
+                        </p>
+                        <p class="mb-0 mt-5">
+                            <a href="" class="active py-2 px-4 ml-0 mr-3 "> <i class="fa fa-check"> </i> MARK AS
+                                SEEN</a>
+                            <a href="" class=" py-2 px-4  mx-3"> <i class="far fa-comment"> </i> ADD COMMENT</a>
+                            <a href="" class=" py-2 px-4 mx-3"> <i class="fas fa-share-alt"> </i> SHARE</a>
+                            <!-- <a href="" class="active"><i class="fa fa-mark ">MARK AS SEEN</i></a>
+                            <a href=""></a><i class="fa fa-comment"></i>ADD COMMENT</a>
+                            <a href=""><i class="fa fa-share"></i>SHARE</a> -->
+                        </p>
+                    </section>
+
+                    <section class="seasons">
+                        <h3>SEASONS</h3>
+                        <ul class="list-unstyled">
+                            <li class="py-2 px-3">Season 8 <i class="fa fa-angle-down"></i></li>
+                            <!-- <ol class="episodes">
+                                  <li>Episode 1</li>
+                                  <li>Episode 2</li>
+                                  <li>Episode 3</li>
+                                  <li>Episode 4</li>
+                                  <li>Episode 5</li>
+                                  <li>Episode 6</li>
+                                  <li>Episode 7</li>
+                                  <li>Episode 8</li>
+                                  <li>Episode 9</li>
+                                  <li>Episode 10</li>
+                              </ol> -->
+
+
+                            <li class="py-2 px-3">Season 7 <i class="fa fa-angle-down"></i></li>
+
+
+
+                            <li class="py-2 px-3">Season 6 <i class="fa fa-angle-down"></i></li>
+
+
+
+                            <li class="py-2 px-3">Season 5 <i class="fa fa-angle-down"></i></li>
+
+
+
+                            <li class="py-2 px-3 ">Season 4 <i class="fa fa-angle-down"></i></li>
+
+
+                            <li class="py-2 px-3">Season 3 <i class="fa fa-angle-down"></i></li>
+
+
+
+                            <li class="py-2 px-3">Season 2 <i class="fa fa-angle-down"></i></li>
+
+
+
+                            <li class="py-2 px-3">Season 1 <i class="fa fa-angle-down"></i></li>
+
+                        </ul>
+                    </section>
+
+                    <section class="episode_thriller row mx-auto my-5">
+                        <div class="episode_thriller_item col-sm-6 col-md-12 col-lg-6">
+                            <iframe src="https://www.youtube.com/embed/zkhUealkTc8/">
+                            </iframe> 
+                            <span>
+                                <a href="">43:05</a>
+                                <a href="">Season 8</a>
+                                <a href="">09/10/2019</a>
+                            </span>
+                            <p>S08 E07 <a>Caballo de Troya</a></p>
+                            <a href="" class="trailer_text">The press decide to blame Inspector Murillo for the shooting of Arturo.</a>
+                        </div>
+                        <div class="episode_thriller_item col-sm-6 col-md-12 col-lg-6">
+                            <iframe src="https://www.youtube.com/embed/zkhUealkTc8/" frameborder="0">
+
+                            </iframe> 
+                            <span>
+                                <a href="">43:05</a>
+                                <a href="">Season 8</a>
+                                <a href="">09/10/2019</a>
+                            </span>
+                            <p>S08 E06 <a>La cálida Guerra Fría
+                            </a></p>
+                            <a href="" class="trailer_text">Raquel's name is raised when details of the negotiations with the robbers are
+                                leaked.</a>
+                        </div>
+                    </section>
+
+                    <section class="comment_container">
+                        <h3>COMMENTS</h3>
+                        <div>
+                        <section class="comment row py-5 px-0">
+                            <i class="fab fa-twitter"> </i>
+                            <article class="t_user col-2 d-flex justify-content-center"><img src="" alt=""> <i class="fas fa-user-circle"></i></article>
+                           
+                            <article class="col-10 tweet">
+                                <p><a class="brand_color">Mark Essien </a> @markessien</p>
+                                <article>
+                                    Game of Thrones is exactly like the title says: A game for the throne. Played by the
+                                    people who think its their right to claim, conquer or inherit it. They all have
+                                    different goals and different ways of getting to that point
+                                    <a class="brand_color">#GOT8 </a>
+                                </article>
+                                <article class="rt_likes my-3">
+                                    <i class="fas fa-retweet mr-5"> &nbsp; 100k</i>
+                                    <i class="fas fa-heart">  &nbsp; 10k</i>
+                                </article>
+                            </article>
+                           
+                        </section> <div class="gists mt-n5 pl-5">
+                                <div class="fa fa-comment pl-5 pt-3"> &nbsp; 120 People are talking about this</div>
+                            </div>
+                        </div>
+                        <div class="pt-5">
+                        <section class="comment row py-5 px-0">
+                            <i class="fab fa-twitter"> </i>
+                            <article class="t_user col-2 d-flex justify-content-center"><img src="" alt=""> <i class="fas fa-user-circle"></i></article>
+                           
+                            <article class="col-10 tweet">
+                                <p><a class="brand_color">Mark Essien </a> @markessien</p>
+                                <article>
+                                    Game of Thrones is exactly like the title says: A game for the throne. Played by the
+                                    people who think its their right to claim, conquer or inherit it. They all have
+                                    different goals and different ways of getting to that point
+                                    <a class="brand_color">#GOT8 </a>
+                                </article>
+                                <article class="rt_likes my-3">
+                                    <i class="fas fa-retweet mr-5"> &nbsp; 100k</i>
+                                    <i class="fas fa-heart">  &nbsp; 10k</i>
+                                </article>
+                            </article>
+                           
+                        </section> 
+                        <div class="gists mt-n5 pl-5">
+                                <div class="fa fa-comment pl-5 pt-3"> &nbsp; 120 People are talking about this</div>
+                        </div>
+                        <p class="pt-3 see_more">See more</p>
+                        </div>
+                    </section>
+                </div>
+            </section>
+        </section>
+
+
+    </main>
+    <?php include "components/site_footer.php" ?>
+</body>
+
+</html>
